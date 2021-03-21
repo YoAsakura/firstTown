1d3d07bdbe8984409c03f955953459a1cac9b7aa
Roman Murunov <54361554+YoAsakura@users.noreply.github.com>
2021-03-21 16:12:43 +0300

    мобилка

--- js/main.js
+++ js/main.js
@@ -3,10 +3,27 @@
 const mobileMenuOpenBtn = document.querySelector('.page-header__burger-btn')
 const mobileMenu = document.querySelector('.page-header__nav')
 const mobileLogoBLock = document.querySelector('.page-header__logo-box')
+const mobileBurgerBtn = document.querySelector('.page-header__burger-btn')
 
-mobileMenuOpenBtn.addEventListener('click', function(){
-    mobileLogoBLock.classList.toggle('page-header__logo-box--blue-back');
-    mobileMenu.classList.toggle('page-header__nav--mobile-open');
+
+function mobileHidden() {
+    if (mobileMenu.classList.contains('page-header__nav--mobile-close') === true) {
+        mobileMenu.classList.remove('page-header__nav--mobile-close')
+    }
+}
+
+
+mobileMenuOpenBtn.addEventListener('click', function () {
+    mobileBurgerBtn.classList.toggle('page-header__burger-btn--close');
+
+    if (mobileMenu.classList.contains('page-header__nav--mobile-open') === true) {
+        mobileMenu.classList.add('page-header__nav--mobile-close')
+        mobileMenu.classList.remove('page-header__nav--mobile-open')
+        setTimeout(mobileHidden, 1000);
+    } else {
+        mobileMenu.classList.remove('page-header__nav--mobile-close')
+        mobileMenu.classList.add('page-header__nav--mobile-open')
+    }
 });
 
- /* --------------------------*/
+/* --------------------------*/
