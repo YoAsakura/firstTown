a8d18834eb83b678352cc9b08e217f066d90ca77
Roman Murunov <54361554+YoAsakura@users.noreply.github.com>
2021-03-23 21:01:45 +0300

    скрипт

--- js/calendar.js
+++ js/calendar.js
@@ -6,25 +6,30 @@ const CalendarBack = document.querySelector('.calendar__event-active-background'
 
 
 for (let i = 0; i < CalendarItemArr.length; i++) {
-    CalendarItemArr[i].addEventListener("click", function(){
+    CalendarItemArr[i].addEventListener("click", function () {
+        CalendarItemArr[i].classList.add('calendar__item--active');
 
-        CalendarEventsArr.forEach(function(item) {
-           item.style.display = "none";
-          });
-
-          if(!CalendarEventsArr[i]){} else {CalendarEventsArr[i].style.display = "block";
-          CalendarBack.style.display = "block";}
+        CalendarEventsArr.forEach(function (item) {
+            item.style.display = "none";
+        });
+        if (!CalendarEventsArr[i]) {} else {
+            CalendarEventsArr[i].style.display = "block";
+            CalendarBack.style.display = "block";
+        }
     });
-  }
+}
 
-  window.addEventListener("keydown", function (event) {
+window.addEventListener("keydown", function (event) {
     if (event.keyCode === 27) {
         CalendarBack.style.display = "none";
-        CalendarEventsArr.forEach(function(item) {
+        CalendarEventsArr.forEach(function (item) {
             item.style.display = "none";
-           });
+            CalendarItemArr.forEach(function (item) {
+                item.classList.remove('calendar__item--active');
+            });
+        });
     }
-  });
+});
 
 
 /* --------------------------*/
