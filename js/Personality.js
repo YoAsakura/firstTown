a02da8fad2ba577f81ce361a8bc7e4ae1f7c4e95
Roman Murunov <54361554+YoAsakura@users.noreply.github.com>
2021-03-26 20:20:40 +0300

    Фикс

---
+++ js/Personality.js
@@ -0,0 +1,7 @@
+const personalityScience = document.querySelectorAll('.personaluty__areas-item');
+
+for ( i = 0; i < personalityScience.length; i++) {
+    personalityScience[i].addEventListener('click', function(){
+        
+    })
+};
