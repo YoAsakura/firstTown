caed46115cc88b30353803320252e7d0f6675503
Roman Murunov <54361554+YoAsakura@users.noreply.github.com>
2021-03-26 03:55:57 +0300

    фикс

--- send.php
+++ send.php
@@ -1,13 +1,17 @@
 <?php
 $fio = $_POST['fio'];
 $email = $_POST['email'];
+$text = $_POST['text'];
 $fio = htmlspecialchars($fio);
 $email = htmlspecialchars($email);
+$text = htmlspecialchars($text);
 $fio = urldecode($fio);
 $email = urldecode($email);
+$text = urldecode($text);
 $fio = trim($fio);
 $email = trim($email);
-if (mail("example@mail.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: romamurunov@Mail.ru \r\n"))
+$text = trim($text);
+if (mail("zzzurking555@mail.ru", "Заявка с сайта ОбнинскГородаПервых", "ФИО:".$fio.". E-mail: ".$email ,"From: romamurunov@mail.ru \r\n"))
  {     echo "сообщение успешно отправлено";
 } else {
     echo "при отправке сообщения возникли ошибки";
